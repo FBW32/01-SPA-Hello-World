29e9c4318b8f11460f9a779d0d5a66aaef68877a
Misbah-D <misbah.deeb@gmail.com>
2020-10-27 09:22:29 +0100

    done

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import react from "react";
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="App">
+   
+      < Header />
+      <h1>Hello World ok</h1>
+    </div>
+  );
+}
+
+export default App;
